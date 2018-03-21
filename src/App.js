66bf36815dc8d625d0253437e0b2a82dc980a478
Idone <idonea@nala.roche.com>
2018-03-21 13:22:32 -0700

    checkpoint-2-listrooms completed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+import RoomList from './components/RoomList';
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAmxchtexfda4Ky6vwrC4gc0FjYvhMTlBw",
+  authDomain: "bloc-chat-react-650ed.firebaseapp.com",
+  databaseURL: "https://bloc-chat-react-650ed.firebaseio.com",
+  projectId: "bloc-chat-react-650ed",
+  storageBucket: "bloc-chat-react-650ed.appspot.com",
+  messagingSenderId: "479271708200"
+};
+firebase.initializeApp(config);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <RoomList firebase={firebase} />
       </div>
     );
   }
